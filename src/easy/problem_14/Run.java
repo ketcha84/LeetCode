69e5af01c262416bb3ad664883039d0ee719633f
Nikita Kachurin <m.c.yasham@gmail.com>
2023-05-21 10:56:44 +0200

    done

--- src/easy/problem_14/Run.java
+++ src/easy/problem_14/Run.java
@@ -2,6 +2,8 @@ package easy.problem_14;
 
 public class Run {
     public static void main(String[] args) {
-
+        String[] strings = {"flower", "flow", "flight"};
+        Solution sol = new Solution();
+        System.out.println(sol.longestCommonPrefix(strings));
     }
 }
--- src/easy/problem_14/Solution.java
+++ src/easy/problem_14/Solution.java
@@ -1,4 +1,32 @@
 package easy.problem_14;
 
 public class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        int minSize = Integer.MAX_VALUE;
+
+        for (int i = 0; i < strs.length; i++) {
+            if (minSize > strs[i].length()) {
+                minSize = strs[i].length();
+            }
+        }
+
+        int indChar = 0;
+        StringBuilder sb = new StringBuilder();
+        while (indChar < minSize) {
+            boolean eq = true;
+            char c = strs[0].charAt(indChar);
+            for (int i = 1; i < strs.length; i++) {
+                if (strs[i].charAt(indChar) != c) {
+                    eq = false;
+                }
+            }
+            if(eq){
+                sb.append(c);
+            }else {
+                return sb.toString();
+            }
+            indChar++;
+        }
+        return sb.toString();
+    }
 }
