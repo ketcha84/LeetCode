c799777a3181ebb62bc3c38cca3c876dc7ed446d
Nikita Kachurin <m.c.yasham@gmail.com>
2023-05-21 09:11:53 +0200

    added calling Solution

--- src/easy/problem_28/Run.java
+++ src/easy/problem_28/Run.java
@@ -1,4 +1,11 @@
 package easy.problem_28;
 
 public class Run {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        String haystack = "sadbutsad";
+        String needle = "sad";
+
+        System.out.println(sol.strStr(haystack, needle));
+    }
 }
