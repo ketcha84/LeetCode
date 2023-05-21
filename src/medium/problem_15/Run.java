0a56f2f212c28406394f9e99917c65f9014752e4
Nikita Kachurin <m.c.yasham@gmail.com>
2023-05-21 12:04:10 +0200

    done

---
+++ src/medium/problem_15/Run.java
@@ -0,0 +1,10 @@
+package medium.problem_15;
+
+public class Run {
+    public static void main(String[] args) {
+        int[] array = new int[]{-1, 0, 1, 2, -1, -4};
+
+        Solution sol = new Solution();
+        System.out.println(sol.threeSum(array));
+    }
+}
---
+++ src/medium/problem_15/Solution.java
@@ -0,0 +1,48 @@
+package medium.problem_15;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> list = new ArrayList<>();
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (nums[i] > 0) {
+                break;
+            }
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            int j = i + 1;
+            int k = nums.length - 1;
+
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum == 0) {
+                    list.add(new ArrayList<>(Arrays.asList(nums[i], nums[j], nums[k])));
+
+                    while (j < k && nums[j] == nums[j + 1]) j++;
+                    while (j < k && nums[k] == nums[k - 1]) k--;
+
+                    j++;
+                    k--;
+                    continue;
+                }
+                if (sum < 0) {
+                    j++;
+                    continue;
+                }
+                if (sum > 0) {
+                    k--;
+                }
+            }
+
+        }
+
+        return list;
+    }
+}
