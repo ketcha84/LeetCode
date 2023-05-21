432c487b05a0d7fb7c240a8a97f9c8f079380656
Nikita Kachurin <m.c.yasham@gmail.com>
2023-05-21 12:13:29 +0200

    done

---
+++ src/medium/problem_50/Run.java
@@ -0,0 +1,8 @@
+package medium.problem_50;
+
+public class Run {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.myPow(2.31,5));
+    }
+}
