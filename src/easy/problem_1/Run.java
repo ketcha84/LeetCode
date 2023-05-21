d4d5675c7f40d56542a544fb153d92d319341615
Nikita Kachurin <m.c.yasham@gmail.com>
2023-05-21 10:43:06 +0200

    done!

--- src/easy/problem_1/Run.java
+++ src/easy/problem_1/Run.java
@@ -1,7 +1,12 @@
 package easy.problem_1;
 
+import java.util.Arrays;
+
 public class Run {
     public static void main(String[] args) {
-
+        Solution sol = new Solution();
+        int[] arr = new int[]{3, 2, 4};
+        int target = 6;
+        System.out.println(Arrays.toString(sol.twoSum(arr, target)));
     }
 }
--- src/easy/problem_1/Solution.java
+++ src/easy/problem_1/Solution.java
@@ -1,5 +1,46 @@
 package easy.problem_1;
 
-public class Solution {
+import java.util.Arrays;
 
+public class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] arr = Arrays.copyOf(nums, nums.length);
+        Arrays.sort(arr);
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (right - left >= 1) {
+            if (arr[left] + arr[right] > target) {
+                right--;
+            }
+            if (arr[left] + arr[right] < target) {
+                left++;
+            }
+
+            if (arr[left] + arr[right] == target) {
+                return searchindexes(arr[left], arr[right], nums);
+
+            }
+        }
+
+
+        return new int[]{0, 0};
+    }
+
+    private static int[] searchindexes(int i, int i1, int[] nums) {
+        int[] result = new int[2];
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] == i) {
+                result[0] = j;
+                break;
+            }
+        }
+        for (int j = nums.length - 1; j > 0; j--) {
+            if (nums[j] == i1) {
+                result[1] = j;
+                break;
+            }
+        }
+        return result;
+    }
 }
