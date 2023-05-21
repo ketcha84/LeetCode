1e9f95203c007ab3d0a5381888b5f57c737e49c6
Nikita Kachurin <m.c.yasham@gmail.com>
2023-05-21 11:01:23 +0200

    done

--- src/easy/problem_20/Run.java
+++ src/easy/problem_20/Run.java
@@ -2,6 +2,7 @@ package easy.problem_20;
 
 public class Run {
     public static void main(String[] args) {
-
+        Solution sol = new Solution();
+        System.out.println(sol.isValid("((){}[])()"));
     }
 }
--- src/easy/problem_20/Solution.java
+++ src/easy/problem_20/Solution.java
@@ -1,5 +1,39 @@
 package easy.problem_20;
 
-public class Solution {
+import java.util.Stack;
 
+public class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            switch (c) {
+                case '(' -> {
+                    stack.push(c);
+                }
+                case ')' -> {
+                    if (stack.size() == 0 || '(' != stack.pop()) {
+                        return false;
+                    }
+                }
+                case '[' -> {
+                    stack.push(c);
+                    break;
+                }
+                case ']' -> {
+                    if (stack.size() == 0 || '[' != stack.pop()) {
+                        return false;
+                    }
+                }
+                case '{' -> {
+                    stack.push(c);
+                }
+                case '}' -> {
+                    if (stack.size() == 0 || '{' != stack.pop()) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return stack.size() == 0;
+    }
 }
