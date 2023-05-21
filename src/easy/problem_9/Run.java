127427b9093d6640ba1ecc7c51625c06260dfb14
Nikita Kachurin <m.c.yasham@gmail.com>
2023-05-21 10:52:31 +0200

    done!

--- src/easy/problem_9/Run.java
+++ src/easy/problem_9/Run.java
@@ -2,6 +2,7 @@ package easy.problem_9;
 
 public class Run {
     public static void main(String[] args) {
-
+        Solution sol = new Solution();
+        System.out.println(sol.isPalindrome(1221));
     }
 }
--- src/easy/problem_9/Solution.java
+++ src/easy/problem_9/Solution.java
@@ -1,4 +1,14 @@
 package easy.problem_9;
 
 public class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0 || (x != 0 && x % 10 == 0))
+            return false;
+        int reverseNum = 0;
+        while (x > reverseNum) {
+            reverseNum = reverseNum * 10 + (x % 10);
+            x /= 10;
+        }
+        return (reverseNum == x || x == reverseNum / 10);
+    }
 }
