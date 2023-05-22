38aa878e8cd051b5ba6540df15abb2907cc206c0
Nikita Kachurin <m.c.yasham@gmail.com>
2023-05-22 19:05:41 +0200

    created

--- src/easy/problem_66/Run.java
+++ src/easy/problem_66/Run.java
@@ -1,7 +1,12 @@
 package easy.problem_66;
 
+import java.util.Arrays;
+
 public class Run {
     public static void main(String[] args) {
+        int[] nums = new int[]{9};
 
+        Solution sol = new Solution();
+        System.out.println(Arrays.toString(sol.plusOne(nums)));
     }
 }
--- src/easy/problem_66/Solution.java
+++ src/easy/problem_66/Solution.java
@@ -2,7 +2,24 @@ package easy.problem_66;
 
 public class Solution {
     public int[] plusOne(int[] digits) {
+        int r = digits.length - 2;
+        digits[digits.length - 1]++;
 
-        return null;
+        while (r >= 0 && digits[r + 1] > 9) {
+            digits[r] = digits[r] + digits[r + 1] / 10;
+            digits[r + 1] = digits[r + 1] % 10;
+            r--;
+        }
+        if (digits[0] > 9) {
+            int[] result = new int[digits.length + 1];
+            result[0] = 1;
+            digits[0] = digits[0] % 10;
+            for (int i = 1; i < result.length; i++) {
+                result[i] = digits[i - 1];
+            }
+            return result;
+        }
+
+        return digits;
     }
 }
