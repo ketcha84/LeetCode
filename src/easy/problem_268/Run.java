376a2940c54ff14f768f6495d36919676c253065
Nikita Kachurin <m.c.yasham@gmail.com>
2023-06-05 20:30:28 +0200

    done

---
+++ src/easy/problem_268/Run.java
@@ -0,0 +1,8 @@
+package easy.problem_268;
+
+public class Run {
+    public static void main(String[] args) {
+
+        System.out.println(new Solution().missingNumber(new int[]{0, 1, 5, 3, 2}));
+    }
+}
---
+++ src/easy/problem_268/Solution.java
@@ -0,0 +1,14 @@
+package easy.problem_268;
+
+public class Solution {
+    public int missingNumber(int[] nums) {
+        int mask = 0;
+        for (int i = 0; i <= nums.length; i++) {
+            mask ^= i;
+        }
+        for (int num : nums) {
+            mask ^= num;
+        }
+        return mask;
+    }
+}
