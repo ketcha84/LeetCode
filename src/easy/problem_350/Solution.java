2e82390b4a7397e7f8367eb0a0487809107d4898
Nikita Kachurin <m.c.yasham@gmail.com>
2023-06-06 21:26:21 +0200

    done

--- src/easy/problem_350/Solution.java
+++ src/easy/problem_350/Solution.java
@@ -5,17 +5,17 @@ import java.util.*;
 public class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
         int[] arr = new int[1001];
-        int[] ans = new int[1001];
+//        int[] ans = new int[Math.min(nums1.length, nums2.length)];
         int count = 0;
         for (int i : nums1) {
             arr[i]++;
         }
         for (int i : nums2) {
             if (arr[i] > 0) {
-                ans[count++] = i;
+                nums1[count++] = i;
                 arr[i]--;
             }
         }
-        return Arrays.copyOfRange(ans, 0, count);
+        return Arrays.copyOfRange(nums1, 0, count);
     }
 }
