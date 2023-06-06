32d3d0e9951b3da105c21b6f748fe0623f7a52c8
Nikita Kachurin <m.c.yasham@gmail.com>
2023-06-06 20:25:09 +0200

    done

--- src/easy/problem_349/Run.java
+++ src/easy/problem_349/Run.java
@@ -5,8 +5,8 @@ import java.util.Arrays;
 public class Run {
     public static void main(String[] args) {
         Solution sol = new Solution();
-        int[] nums1 = {4, 9, 5};
-        int[] nums2 = {9, 4, 9, 8, 4};
+        int[] nums1 = {1,2,2,1};
+        int[] nums2 = {2,2};
         System.out.println(Arrays.toString(sol.intersection(nums1, nums2)));
     }
 }
