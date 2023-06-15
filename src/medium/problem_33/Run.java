636110dee1a027f15902575b8d024dc807676c89
Nikita Kachurin <m.c.yasham@gmail.com>
2023-06-15 19:48:23 +0200

    done

---
+++ src/medium/problem_33/Run.java
@@ -0,0 +1,10 @@
+package medium.problem_33;
+
+public class Run {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int[] arr = {4, 5, 6, 7, 0, 1, 2};
+
+        System.out.println(sol.search(arr, 2));
+    }
+}
---
+++ src/medium/problem_33/Solution.java
@@ -0,0 +1,33 @@
+package medium.problem_33;
+
+public class Solution {
+    public int search(int[] nums, int target) {
+        int l = 0;
+        int r = nums.length - 1;
+        int m;
+
+        while (l <= r) {
+            m = (int) Math.floor((l + r) / 2);
+
+            if (nums[m] == target) {
+                return m;
+            }
+
+            if (nums[l] <= nums[m]) {
+                if (nums[l] <= target && target <= nums[m]) {
+                    r = m - 1;
+                } else {
+                    l = m + 1;
+                }
+            } else {
+                if (nums[m] <= target && target <= nums[r]) {
+                    l = m + 1;
+                } else {
+                    r = m - 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+}
