603d8597ac6abbff5a0c72f60763f067a0137833
Nikita Kachurin <m.c.yasham@gmail.com>
2023-06-17 00:43:54 +0200

    done

--- src/medium/problem_74/Run.java
+++ src/medium/problem_74/Run.java
@@ -2,8 +2,10 @@ package medium.problem_74;
 
 public class Run {
     public static void main(String[] args) {
-        int[][] mat = {{1}};
+        int[][] mat = {{1, 3, 5, 7},
+                {10, 11, 16, 20},
+                {23, 30, 34, 60}};
         Solution sol = new Solution();
-        System.out.println(sol.searchMatrix(mat, 1));
+        System.out.println(sol.searchMatrix2(mat, 17));
     }
 }
