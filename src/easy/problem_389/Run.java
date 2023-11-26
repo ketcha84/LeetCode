f031c9eb5efc8ac7a99450f453ddda655712b9fd
Nikita Kachurin <kachurin.nikita84@gmail.com>
2023-11-26 16:28:11 +0100

    done

---
+++ src/easy/problem_389/Run.java
@@ -0,0 +1,8 @@
+package easy.problem_389;
+
+public class Run {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.findTheDifference("abc","abcd"));
+    }
+}
