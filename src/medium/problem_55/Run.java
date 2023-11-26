a5424844d8fb77147352bb68da197c252e1dcbc5
Nikita Kachurin <kachurin.nikita84@gmail.com>
2023-11-26 17:18:43 +0100

    done

---
+++ src/medium/problem_55/Run.java
@@ -0,0 +1,9 @@
+package medium.problem_55;
+
+public class Run {
+    public static void main(String[] args) {
+        int[] arr = {1,0,1,0,4};
+        Solution sol = new Solution();
+        System.out.println(sol.canJump(arr));
+    }
+}
---
+++ src/medium/problem_55/Solution.java
@@ -0,0 +1,12 @@
+package medium.problem_55;
+
+public class Solution {
+    public boolean canJump(int[] nums) {
+        int reach = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (i > reach) return false;
+            reach = Math.max(reach, i + nums[i]);
+        }
+        return true;
+    }
+}
